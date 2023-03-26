6f0aa909d19f9996c244cbb66ff7580ce23fb97e
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2023-03-26 15:07:27 +0300

    add task

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const totalCategories = document.querySelectorAll(".item");
+console.log ('Number of categories: ${totalCategories}')
+
+
+
+// const totalCategories = document.querySelectorAll(".item");
+// console.log(`В списке ${totalCategories.length} категории.`);
+
+// const categoriesArray = [...totalCategories]
+//   .map(
+//     categories => `Категория: ${categories.children[0].textContent}
+// Количество элементов: ${categories.children[1].children.length}`
+//   )
+//   .join("\n");
+// console.log(categoriesArray);
